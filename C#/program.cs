9a901dfb2445235b8709784ff30d699721bf7598
RatRatARMY <156580271+kalksdlkdsa@users.noreply.github.com>
2024-02-03 22:04:24 +0700

    My C# file

---
+++ C#/program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DateTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime latestDate = new DateTime(9999, 12, 31, 23, 59, 59, 999);
+            Console.WriteLine(latestDate);
+            Console.Read();
+        }
+    }
+}
